b9abe24aaec191609e9707abdf69602b37acdfac
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-25 13:53:25 +0000

    cubes

--- src/Componects.jsx
+++ src/Componects.jsx
@@ -1,14 +1,28 @@
 import { Canvas } from "@react-three/fiber"
 
+const Cube = ({position, size, color}) => {
+    return (
+    <mesh position={position}>
+    <boxGeometry args={size} />
+    <meshStandardMaterial color={color}/>
+</mesh>
+)
+}
 
 const Component = () => {
+
+    
     return (
         <Canvas>
-            <mesh>
-                <boxGeometry args={[2, 2, 4]}>
-                    <meshStandardMaterial/>
-                </boxGeometry>
-            </mesh>
+
+        <directionalLight position={[0, 0, 2]} />
+
+            <Cube position={[1,0,0]} color={"green"} size={[1,1,1]}/>
+            <Cube position={[-1,0,0]} color={"red"} size={[1,1,1]}/>
+            <Cube position={[1,2,0]} color={"yellow"} size={[1,1,1]}/>
+            <Cube position={[-1,2,-2]} color={"green"} size={[1,1,1]}/>
+          
+           
         </Canvas>
     )
 }

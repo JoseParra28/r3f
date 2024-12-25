317e6203f33c1612aee324eb48355e43be92dcb9
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-25 14:01:15 +0000

    group

--- src/Componects.jsx
+++ src/Componects.jsx
@@ -1,4 +1,5 @@
 import { Canvas } from "@react-three/fiber"
+import { AmbientLight } from "three"
 
 const Cube = ({position, size, color}) => {
     return (
@@ -16,12 +17,13 @@ const Component = () => {
         <Canvas>
 
         <directionalLight position={[0, 0, 2]} />
-
+        <ambientLight intensity={[0.4]}/>
+        <group position={[0,-1,0]}>
             <Cube position={[1,0,0]} color={"green"} size={[1,1,1]}/>
             <Cube position={[-1,0,0]} color={"red"} size={[1,1,1]}/>
             <Cube position={[1,2,0]} color={"yellow"} size={[1,1,1]}/>
             <Cube position={[-1,2,-2]} color={"green"} size={[1,1,1]}/>
-          
+        </group>
            
         </Canvas>
     )

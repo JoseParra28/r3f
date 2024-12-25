cb8056688a5ed6a4b99d9c3c6921e39709788690
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-25 15:26:06 +0000

    more elements

--- src/Componects.jsx
+++ src/Componects.jsx
@@ -1,5 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber"
 import { useRef } from "react"
+import { SphereGeometry } from "three"
 
 
 const Cube = ({position, size, color}) => {
@@ -20,23 +21,38 @@ const Cube = ({position, size, color}) => {
 </mesh>
 )
 }
+const Sphere = ({position, size, color}) => {
+    return (
+        <mesh position={position}>
+            <sphereGeometry args={size}/>
+            <meshStandardMaterial color={color}/>
+        </mesh>
+    )
+}
 
+const Torus = ({position, size, color}) => {
+    return (
+        <mesh position={position}>
+            <torusGeometry args={size}/>
+            <meshStandardMaterial color={color}/>
+        </mesh>
+    )
+}
 const Component = () => {
-
-    
     return (
         <Canvas>
-
         <directionalLight position={[0, 0, 2]} />
-
         <ambientLight intensity={[0.4]}/>
-
+{
         <group position={[-2,-1,0]}>
-            <Cube position={[1,0,0]} color={"green"} size={[1,1,1]}/>
+            {/* <Cube position={[1,0,0]} color={"green"} size={[1,1,1]}/>
             <Cube position={[-1,0,0]} color={"red"} size={[1,1,1]}/>
             <Cube position={[1,2,0]} color={"yellow"} size={[1,1,1]}/>
             <Cube position={[-1,2,-2]} color={"green"} size={[1,1,1]}/>
-        </group>
+            <Sphere position={[0,0,0]} args={[0,30,30]} color={"green"}/> */}
+        </group> } 
+            <Torus position={[0,0,0]} args={[0,30,30]} color={"gray"}/>
+        
         </Canvas>
     )
 }
